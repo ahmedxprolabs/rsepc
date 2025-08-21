13b37ba1ba0302468e18d9f4957a755dc7b87cd6
ahmedxprolabs <ahmed.rasul@xprolabs.com>
2025-08-21 19:45:44 +0500

    rspec initial

--- db/migrate/20250821144043_create_comments.rb
+++ db/migrate/20250821144043_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[8.0]
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
       t.text :content, null: false
-
       t.timestamps
     end
   end
