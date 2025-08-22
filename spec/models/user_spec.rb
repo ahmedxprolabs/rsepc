f610dc70153175757674eb23e08c3e2b1b55e5ff
ahmedxprolabs <ahmed.rasul@xprolabs.com>
2025-08-22 14:54:18 +0500

    rspec implementation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).case_insensitive }
-  it { should have_many(:posts).dependent(:destroy) }
-  it { should have_many(:comments).dependent(:destroy) }
-
   it "has a valid factory" do
     expect(build(:user)).to be_valid
   end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,29 @@
-require 'rails_helper'
-
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
     it "renders index successfully" do
       create_list(:post, 2)
       get posts_path
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include(Post.first.title)
     end
   end
 
   describe "POST /posts" do
-    it "creates a post" do
-      user = create(:user)
+    let(:user) { create(:user) }
 
+    it "creates a post with valid params" do
       expect {
         post posts_path, params: { post: { user_id: user.id, title: "New Post", body: "Hello" } }
       }.to change(Post, :count).by(1)
 
       expect(response).to redirect_to(post_path(Post.last))
+    end
+
+    it "does not create a post with invalid params" do
+      expect {
+        post posts_path, params: { post: { user_id: user.id, title: "", body: "" } }
+      }.not_to change(Post, :count)
+
+      expect(response.body).to include("error") # assumes error messages rendered
     end
   end
 end
