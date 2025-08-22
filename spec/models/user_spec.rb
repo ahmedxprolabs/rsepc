659d7430101494eb8deb6418dc959bbbf3cd9f8f
ahmedxprolabs <ahmed.rasul@xprolabs.com>
2025-08-22 12:42:02 +0500

    rspec first test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should have_many(:posts).dependent(:destroy) }
+  it { should have_many(:comments).dependent(:destroy) }
+
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
 end
